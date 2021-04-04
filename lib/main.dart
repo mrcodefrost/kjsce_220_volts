b3f26a43dff3009fdc970398b0f4210bf9c9f5a9
mrcodefrost <72447688+mrcodefrost@users.noreply.github.com>
2021-04-04 09:16:26 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kjsce_220_volts/screens/kj_form.dart';
+import 'package:kjsce_220_volts/screens/kj_welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         textTheme: GoogleFonts.muktaVaaniTextTheme(),
       ),
-      home: FormScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
